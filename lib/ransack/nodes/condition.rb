0ffdf9f8b314a2de97041c19e4d27fdec68ecaff
Matt Blaul <matt.blaul@kadince.com>
2024-01-15 08:58:31 -0500

    Ensuring not_in predicates filter association correctly

--- lib/ransack/nodes/condition.rb
+++ lib/ransack/nodes/condition.rb
@@ -293,7 +293,11 @@ module Ransack
               query.where(format_predicate(attribute))
               Arel::Nodes::In.new(context.primary_key, Arel.sql(query.to_sql))
             else
-              query.where(format_predicate(attribute).not)
+              replace_gsub = ['not_', '']
+              if self.predicate_name == 'does_not_match'
+                replace_gsub = ['does_not_match', 'matches']
+              end
+              query = context.klass.ransack("#{attribute.name}_#{self.predicate.name.gsub(*replace_gsub)}" => self.value).result.select(:id)
               Arel::Nodes::NotIn.new(context.primary_key, Arel.sql(query.to_sql))
             end
           else
